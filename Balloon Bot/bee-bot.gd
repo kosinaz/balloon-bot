d15bfd408c6a3c9a09543518c19015c9079fa846
Kosina Zoltán <kosinaz@gmail.com>
2024-08-29 01:12:36 +0200

    add navagent

--- Balloon Bot/bee-bot.gd
+++ Balloon Bot/bee-bot.gd
@@ -1,9 +1,10 @@
 extends KinematicBody2D
 
 func _physics_process(_delta):
-	if $"../BalloonBot".position.distance_to(position) < 150:
+#	if $"../BalloonBot".position.distance_to(position) < 150:
 # warning-ignore:return_value_discarded
-		move_and_slide(($"../BalloonBot".position - position).normalized() * 100, Vector2.UP)
+		$NavigationAgent2D.set_target_location($"../BalloonBot".global_position)
+		move_and_slide(global_position.direction_to($NavigationAgent2D.get_next_location()) * 100)
 		$Sprite.flip_h = $"../BalloonBot".position.x > position.x
 
 func _on_Timer_timeout():
